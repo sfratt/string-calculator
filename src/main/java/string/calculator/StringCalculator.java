d0a3eb76039291c4a33e8f524a5888070bcbebbe
sfratt <sfratt@users.noreply.github.com>
2019-09-15 11:08:15 -0400

    Implement addition in `add` method
    Change `add` to add parsed integers and return value for up to 2 numbers

--- src/main/java/string/calculator/StringCalculator.java
+++ src/main/java/string/calculator/StringCalculator.java
@@ -3,17 +3,18 @@ package string.calculator;
 public class StringCalculator {
     
     public static final int add(final String numbers) {
+        int returnValue = 0;
         String[] numbersArray = numbers.split(",");
         
         if (numbersArray.length > 2) {
             throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
         } else {
             for (String number : numbersArray) {
-                if (!number.isEmpty()) {
-                    Integer.parseInt(number);
+                if (!number.trim().isEmpty()) {
+                    returnValue += Integer.parseInt(number);
                 }
             }
         }
-        return 0;
+        return returnValue;
     }
 }
