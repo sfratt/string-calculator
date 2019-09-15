293980f58a586372d91f2a760b6e895afa9639fa
sfratt <sfratt@users.noreply.github.com>
2019-09-15 01:33:32 -0400

    Implement unit tests and calculator `add` method
    Ensure that `add` method is implemented and can take between 0-2 numbers
    as arguments seperated by a comma.

---
+++ src/test/java/string/calculator/StringCalculatorTest.java
@@ -0,0 +1,22 @@
+package string.calculator;
+
+import org.junit.*;
+
+public class StringCalculatorTest {
+    
+    @Test(expected = RuntimeException.class)
+    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,2,3");
+    }
+
+    @Test
+    public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
+        StringCalculator.add("1,2");
+        Assert.assertTrue(true);
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,X");
+    }
+}
