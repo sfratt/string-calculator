73e715004812ed867f5395798a77e59e05a10312
sfratt <sfratt@users.noreply.github.com>
2019-09-15 12:18:41 -0400

    Modify `add` to handle unknown amount of numbers
    Change to build.gradle indicates that the main class is StringCalculator
    instead of Gradle default "App".
    
    Azure Pipelines now builds and tests the code across Windows, Linux, and
    macOS agents.

--- src/test/java/string/calculator/StringCalculatorTest.java
+++ src/test/java/string/calculator/StringCalculatorTest.java
@@ -4,11 +4,6 @@ import org.junit.*;
 
 public class StringCalculatorTest {
     
-    @Test(expected = RuntimeException.class)
-    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
-        StringCalculator.add("1,2,3");
-    }
-
     @Test
     public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
         StringCalculator.add("1,2");
@@ -33,5 +28,10 @@ public class StringCalculatorTest {
     @Test
     public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
         Assert.assertEquals(3+6, StringCalculator.add("3,6"));
+    }
+
+    @Test
+    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
+        Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
     }
 }
